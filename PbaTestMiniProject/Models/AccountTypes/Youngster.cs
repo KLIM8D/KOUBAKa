c4369a150ad1d25d2a7bb9e1b8db386fd735ccaa
Morten Reetz-Engell <=>
2015-05-19 09:59:30 +0200

    Teh project

---
+++ PbaTestMiniProject/Models/AccountTypes/Youngster.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.AccountTypes
+{
+    class Youngster : Account
+    {
+        public override bool Withdraw(double amount, string text)
+        {
+            if (amount <= 0)
+                return false;
+
+            if (amount <= base.Balance)
+            {
+                var t = new Transaction
+                {
+                    Amount = amount,
+                    TransactionText = text,
+                    Type = TransactionType.Withdraw,
+                    TimeOfTransaction = DateTime.Now
+                };
+                Transactions.Add(t);
+                Balance -= amount;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public override bool Deposit(double amount, string text)
+        {
+            if (amount <= 0)
+                return false;
+
+            if (amount <= base.Balance)
+            {
+                var t = new Transaction
+                {
+                    Amount = amount,
+                    TransactionText = text,
+                    Type = TransactionType.Deposit,
+                    TimeOfTransaction = DateTime.Now
+                };
+                Transactions.Add(t);
+                Balance += amount;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public override bool AddInterest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PbaTestMiniProject/Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Annotations;
+
+namespace Models
+{
+    public enum TransactionType
+    {
+        Deposit, Withdraw
+    }
+
+    public class Transaction
+    {
+        public TransactionType Type { get; set; }
+        public double Amount { get; set; }
+        public string TransactionText { get; set; }
+        public DateTime TimeOfTransaction { get; set; }
+    }
+}
